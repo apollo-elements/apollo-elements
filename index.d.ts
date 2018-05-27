e97fe4664c60b2ffb479e2d48b76294a5a9f2076
Benny Powers <web@bennypowers.com>
2018-05-27 21:55:56 +0300

    Adds some declarations

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,25 @@
+import { DocumentNode } from 'graphql';
 declare global {
   interface Window { __APOLLO_CLIENT__?: any; }
 }
 
-export function hasAllVariables(any): Function(any)
+declare global {
+  interface Window { __APOLLO_CLIENT__?: any; }
+}
+
+declare function hasAllVariables(params: any): (document: DocumentNode) => Boolean
+
+declare class ApolloElement extends HTMLElement {
+  constructor();
+  client: any;
+}
+
+declare class ApolloQuery extends ApolloElement {
+  constructor();
+  query: DocumentNode;
+}
+
+declare class ApolloMutation extends ApolloElement {
+  constructor();
+  mutation: DocumentNode;
+}
