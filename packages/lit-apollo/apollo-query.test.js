c13b677cc398dcf5a257b81c4ed0091154451cd0
Benny Powers <web@bennypowers.com>
2019-01-21 00:29:18 +0200

    Adds tests for lit-element rendering

--- packages/lit-apollo/apollo-query.test.js
+++ packages/lit-apollo/apollo-query.test.js
@@ -1,32 +1,70 @@
-import { chai, expect, html } from '@open-wc/testing';
-import { ifDefined } from 'lit-html/directives/if-defined';
+import { chai, defineCE, fixture, unsafeStatic, expect, html } from '@open-wc/testing';
 import sinonChai from 'sinon-chai';
 
 import { ApolloQuery } from './apollo-query';
-import { getElementWithLitTemplate, hasGetterSetter } from '@apollo-elements/test-helpers/helpers';
 
 chai.use(sinonChai);
 
-const scriptTemplate = script => html`<script type="application/graphql">${script}</script>`;
-const getClass = () => ApolloQuery;
-const getTemplate = (tag, { query, variables, client, script } = {}) => html`
-  <${tag} .client="${ifDefined(client)}">
-    ${script && scriptTemplate(script)}
-  </${tag}>`;
-
-const getElement = getElementWithLitTemplate({ getClass, getTemplate });
-
 describe('ApolloQuery', function describeApolloQuery() {
-  it('defines observed properties', async function definesObserveProperties() {
-    const el = await getElement();
-    expect(hasGetterSetter(el, 'networkStatus'), 'networkStatus').to.be.true;
-    expect(hasGetterSetter(el, 'query'), 'query').to.be.true;
-    expect(hasGetterSetter(el, 'data'), 'data').to.be.true;
-    expect(hasGetterSetter(el, 'error'), 'error').to.be.true;
-    expect(hasGetterSetter(el, 'loading'), 'loading').to.be.true;
-    expect(hasGetterSetter(el, 'client'), 'client').to.be.true;
-    // NB: the following is just to satisfy istanbul. k whatev
-    el.normalProperty = 'foo';
-    expect(hasGetterSetter(el, 'normalProperty')).to.be.false;
+  it('renders when networkStatus is set', async function rendersOnNetworkStatus() {
+    const tagName = defineCE(class extends ApolloQuery {
+      render() {
+        const { networkStatus = 0 } = this;
+        return html`${networkStatus ? 'SUCCESS' : 'FAIL'}`;
+      }
+
+      shouldUpdate() {
+        // by default, ApolloQuery only updates
+        // if data error or loading are set
+        return true;
+      }
+    });
+    const tag = unsafeStatic(tagName);
+    const el = await fixture(html`<${tag} networkStatus="1"></${tag}>`);
+    expect(el).shadowDom.to.equal('SUCCESS');
+  });
+
+  it('does not render by default', async function noRender() {
+    const tagName = defineCE(class extends ApolloQuery {
+      render() {
+        return html`RENDERED`;
+      }
+    });
+    const tag = unsafeStatic(tagName);
+    const el = await fixture(html`<${tag}></${tag}>`);
+    expect(el).shadowDom.to.equal('');
+  });
+
+  it('does render when data is set', async function dataRender() {
+    const tagName = defineCE(class extends ApolloQuery {
+      render() {
+        return html`${this.data.test}`;
+      }
+    });
+    const tag = unsafeStatic(tagName);
+    const el = await fixture(html`<${tag} .data="${{ test: 'RENDERED' }}"></${tag}>`);
+    expect(el).shadowDom.to.equal('RENDERED');
+  });
+
+  it('does render when error is set', async function errorRender() {
+    const tagName = defineCE(class extends ApolloQuery {
+      render() {
+        return html`${this.error}`;
+      }
+    });
+    const tag = unsafeStatic(tagName);
+    const el = await fixture(html`<${tag} .error="${'ERROR'}"></${tag}>`);
+    expect(el).shadowDom.to.equal('ERROR');
+  });
+
+  it('does render when loading is set', async function loadingRender() {
+    const tagName = defineCE(class extends ApolloQuery {
+      render() {
+        return html`${this.loading ? 'SUCCESS' : 'FAIL'}`;
+      }
+    });
+    const tag = unsafeStatic(tagName);
+    const el = await fixture(html`<${tag} loading></${tag}>`);
+    expect(el).shadowDom.to.equal('SUCCESS');
   });
 });
