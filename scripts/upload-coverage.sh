f81b85f3128897747b1cf14fb62b2041e44865e4
Benny Powers <web@bennypowers.com>
2019-03-05 20:56:49 +0200

    chore: fix coverage upload

--- scripts/upload-coverage.sh
+++ scripts/upload-coverage.sh
@@ -1,2 +1,10 @@
-./cc-test-reporter sum-coverage codeclimate.*.json -p 2 -o codeclimate.total.json
-./cc-test-reporter upload-coverage -i codeclimate.total.js
+if [ "$CIRCLE_PULL_REQUEST" == "false" ]; then
+    for f in packages/*; do
+      if [ -d $f -a -d $f/coverage ]; then
+        echo $f;
+        ./cc-test-reporter format-coverage -t lcov -o coverage/coverage.${f//\//-}.json $f/coverage/lcov.info;
+      fi
+    done;
+    ./cc-test-reporter sum-coverage -o coverage/coverage.total.json coverage/coverage.*.json;
+    ./cc-test-reporter upload-coverage -i coverage/coverage.total.json;
+  fi
