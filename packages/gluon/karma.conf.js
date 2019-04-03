93f3300ae4ba39c45cd8a2520ebed13ffd50649c
Benny Powers <benny.powers@forter.com>
2019-04-03 13:56:09 +0300

    test: unify karma tests at repo root

--- packages/gluon/karma.conf.js
+++
@@ -1,8 +0,0 @@
-const defaultSettings = require('@open-wc/testing-karma/default-settings.js');
-const merge = require('webpack-merge');
-
-module.exports = config => {
-  const files = config.grep ? [config.grep] : ['./*.test.js'];
-  config.set(merge(defaultSettings(config), { files }));
-  return config;
-};
--- scripts/upload-coverage.sh
+++ scripts/upload-coverage.sh
@@ -1,10 +1,9 @@
 if [ "$CIRCLE_PULL_REQUEST" == "false" ]; then
-    for f in packages/*; do
-      if [ -d $f -a -d $f/coverage ]; then
-        echo $f;
-        ./cc-test-reporter format-coverage -t lcov -o coverage/coverage.${f//\//-}.json $f/coverage/lcov.info;
-      fi
-    done;
-    ./cc-test-reporter sum-coverage -o coverage/coverage.total.json coverage/coverage.*.json;
-    ./cc-test-reporter upload-coverage -i coverage/coverage.total.json;
-  fi
+  echo "Formatting Coverage Reports..."
+  ./cc-test-reporter format-coverage -t lcov -o coverage/coverage.total.json coverage/lcov.info;
+
+  echo "Uploading Coverage Reports..."
+  ./cc-test-reporter upload-coverage -i coverage/coverage.total.json;
+
+  rm -f cc-test-reporter
+fi
