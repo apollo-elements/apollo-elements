36cf8df4bb237e2da4fe43b704fd06130678b64f
Benny Powers <web@bennypowers.com>
2020-01-09 20:34:57 +0200

    fix(gluon): add missing export to index.js

--- packages/gluon/index.d.ts
+++ packages/gluon/index.d.ts
@@ -1,6 +1,12 @@
+// Classes
 export { ApolloElement } from "./apollo-element";
 export { ApolloQuery } from "./apollo-query";
 export { ApolloMutation } from "./apollo-mutation";
 export { ApolloSubscription } from "./apollo-subscription";
+
+// Utilities
+export { render, html } from 'lit-html';
+
+// Types
 export { MutationResult } from "@apollo-elements/mixins/apollo-mutation-mixin";
 export { OnSubscriptionDataOptions, SubscriptionResult } from "@apollo-elements/mixins/apollo-subscription-mixin";
--- packages/gluon/index.js
+++ packages/gluon/index.js
@@ -1,4 +1,5 @@
 // Classes
+export { ApolloElement } from './apollo-element.js';
 export { ApolloQuery } from './apollo-query.js';
 export { ApolloMutation } from './apollo-mutation.js';
 export { ApolloSubscription } from './apollo-subscription.js';
