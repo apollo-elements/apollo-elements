c7fdb99ff8586735fc7ae4fb9504725e2cf16d7f
Benny Powers <web@bennypowers.com>
2020-01-09 20:36:08 +0200

    fix(mixins): add missing exports to index and .d.ts

--- packages/mixins/index.d.ts
+++ packages/mixins/index.d.ts
@@ -1,4 +1,4 @@
 export { ApolloElementMixin } from "./apollo-element-mixin";
-export { ApolloMutationMixin } from "./apollo-mutation-mixin";
 export { ApolloQueryMixin } from "./apollo-query-mixin";
+export { ApolloMutationMixin } from "./apollo-mutation-mixin";
 export { ApolloSubscriptionMixin } from "./apollo-subscription-mixin";
--- packages/mixins/index.js
+++ packages/mixins/index.js
@@ -1,3 +1,4 @@
+export { ApolloElementMixin } from './apollo-element-mixin.js';
 export { ApolloQueryMixin } from './apollo-query-mixin.js';
 export { ApolloMutationMixin } from './apollo-mutation-mixin.js';
 export { ApolloSubscriptionMixin } from './apollo-subscription-mixin.js';
