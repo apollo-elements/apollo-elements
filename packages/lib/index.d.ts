20e4acc23c6aba3001077dc4f586f4c3a8e1477e
Benny Powers <web@bennypowers.com>
2020-01-09 20:35:19 +0200

    fix(lib): add missing exports to .d.ts

--- packages/lib/index.d.ts
+++ packages/lib/index.d.ts
@@ -1,4 +1,5 @@
 export { default as hasAllVariables } from './has-all-variables';
+export { default as getGraphQLScriptChildDocument } from './get-graphql-script-child-document.js';
 export { default as gqlFromInnerText } from './gql-from-inner-text';
 export { default as isClientOperation } from './is-client-operation';
 export { default as isValidGql } from './is-valid-gql';
