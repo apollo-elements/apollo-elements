94abec5f7e01f2a3b51fee82dc5222503b2825de
Benny Powers <web@bennypowers.com>
2020-08-31 17:15:58 +0300

    feat(lib): apollo client 3
    - perf: remove crocks dependency
      but add apollo-utilities
    
    BREAKING CHANGES:
    - bump to `@apollo/client`
    - remove default exports

--- packages/lib/gql-from-inner-text.test.ts
+++ packages/lib/gql-from-inner-text.test.ts
@@ -1,55 +1,90 @@
 import { expect } from '@open-wc/testing';
 
-import gql from 'graphql-tag';
-import gqlFromInnerText from './gql-from-inner-text';
+import { gqlFromInnerText } from './gql-from-inner-text';
 
-const query = 'query { one }';
+import gql from 'graphql-tag';
+import NoParamQuery from '@apollo-elements/test-helpers/NoParam.query.graphql';
+
+const query = NoParamQuery.loc.source.body;
 const expectedGql = gql(query);
 
-const testDom = document.createElement('div');
-testDom.innerHTML = `
-  <script id="valid" type="application/graphql">${query}</script>
-  <script id="invalid-type" type="application/json">${query}</script>
-  <script id="no-type">${query}</script>
-  <div id="invalid-tag">${query}</div>
-`;
-
-const validElement = testDom.querySelector('#valid') as HTMLScriptElement;
-const invalidType = testDom.querySelector('#invalid-type') as HTMLScriptElement;
-const noType = testDom.querySelector('#no-type') as HTMLScriptElement;
-const invalidTag = testDom.querySelector('#invalid-tag');
-const errorRegexp = /Script must be of type "application\/graphql"/;
+const ERROR_REGEXP = /script must be of type "application\/graphql"/;
 
 describe('[lib] gqlFromInnerText', function describeGqlFromInnerText() {
-  it('returns with valid element', function() {
-    expect(() => gqlFromInnerText(validElement)).to.not.throw;
-    expect(gqlFromInnerText(validElement)).to.equal(expectedGql);
+  let s: HTMLScriptElement;
+
+  afterEach(function() {
+    s?.remove();
+    s = undefined;
   });
 
-  it('throws with invalid type attribute', function() {
-    expect(() => gqlFromInnerText(invalidType)).to.throw(errorRegexp);
+  describe('with valid element', function() {
+    beforeEach(function() {
+      s = document.createElement('script');
+      s.setAttribute('type', 'application/graphql');
+      s.textContent = query;
+    });
+
+    it('does not throw', function() {
+      expect(() => gqlFromInnerText(s)).to.not.throw;
+    });
+
+    it('returns DocumentNode', function() {
+      expect(gqlFromInnerText(s)).to.equal(expectedGql);
+    });
   });
 
-  it('throws with no type attribute', function() {
-    expect(() => gqlFromInnerText(noType)).to.throw(errorRegexp, 'Throws expected error');
+  describe('with invalid type attribute', function() {
+    beforeEach(function() {
+      s = document.createElement('script');
+      s.setAttribute('type', 'application/json');
+      s.textContent = query;
+    });
+
+    it('throws an error', function() {
+      expect(() => gqlFromInnerText(s)).to.throw(ERROR_REGEXP, 'Throws expected error');
+    });
   });
 
-  it('throws with invalid tag', function() {
-    expect(() => gqlFromInnerText(
-      // overriding type since that's the test
-      invalidTag as HTMLScriptElement
-    )).to.throw(errorRegexp, 'Throws expected error');
+  describe('with no type attribute', function() {
+    beforeEach(function() {
+      s = document.createElement('script');
+      s.textContent = query;
+    });
+
+    it('throws an error', function() {
+      expect(() => gqlFromInnerText(s)).to.throw(ERROR_REGEXP, 'Throws expected error');
+    });
+  });
+
+  describe('with invalid tag', function() {
+    beforeEach(function() {
+      s = document.createElement('div');
+      s.textContent = query;
+    });
+
+    it('throws an error', function() {
+      // @ts-expect-error: testing for error.
+      expect(() => gqlFromInnerText(s)).to.throw(ERROR_REGEXP, 'Throws expected error');
+    });
   });
 
   it('handles weird input', function() {
-    expect(gqlFromInnerText(void 0), 'no params').to.be.null;
-    expect(gqlFromInnerText(null), 'null params').to.be.null;
-    expect(gqlFromInnerText(NaN as any), 'NaN params').to.be.null;
-    expect(() => gqlFromInnerText({} as any), 'undefined params').to.throw(errorRegexp);
-    expect(() => gqlFromInnerText(1 as any), 'Number params').to.throw(errorRegexp);
-    expect(() => gqlFromInnerText(true as any), 'Boolean params').to.throw(errorRegexp);
-    expect(() => gqlFromInnerText('foo' as any), 'String params').to.throw(errorRegexp);
-    expect(() => gqlFromInnerText((() => 'hmm') as any), 'Function params').to.throw(errorRegexp);
-    expect(() => gqlFromInnerText(Promise.resolve() as any), 'Promise params').to.throw(errorRegexp);
+    expect(() => gqlFromInnerText(void 0), 'no params').to.throw(ERROR_REGEXP);
+    expect(() => gqlFromInnerText(null), 'null params').to.throw(ERROR_REGEXP);
+    // @ts-expect-error: testing for error.
+    expect(() => gqlFromInnerText(NaN), 'NaN params').to.throw(ERROR_REGEXP);
+    // @ts-expect-error: testing for error.
+    expect(() => gqlFromInnerText({}), 'undefined params').to.throw(ERROR_REGEXP);
+    // @ts-expect-error: testing for error.
+    expect(() => gqlFromInnerText(1), 'Number params').to.throw(ERROR_REGEXP);
+    // @ts-expect-error: testing for error.
+    expect(() => gqlFromInnerText(true), 'Boolean params').to.throw(ERROR_REGEXP);
+    // @ts-expect-error: testing for error.
+    expect(() => gqlFromInnerText('foo'), 'String params').to.throw(ERROR_REGEXP);
+    // @ts-expect-error: testing for error.
+    expect(() => gqlFromInnerText((() => 'hmm')), 'Function params').to.throw(ERROR_REGEXP);
+    // @ts-expect-error: testing for error.
+    expect(() => gqlFromInnerText(Promise.resolve()), 'Promise params').to.throw(ERROR_REGEXP);
   });
 });
--- packages/lib/gql-from-inner-text.ts
+++ packages/lib/gql-from-inner-text.ts
@@ -1,47 +1,21 @@
-import gql from 'graphql-tag';
-import constant from 'crocks/combinators/constant';
-import compose from 'crocks/helpers/compose';
-import propOr from 'crocks/helpers/propOr';
-import isSame from 'crocks/predicates/isSame';
-import isNil from 'crocks/predicates/isNil';
-import map from 'crocks/pointfree/map';
-import option from 'crocks/pointfree/option';
-import and from 'crocks/logic/and';
-import ifElse from 'crocks/logic/ifElse';
-import prop from 'crocks/Maybe/prop';
-import { replace } from './helpers';
 import type { DocumentNode } from 'graphql';
 
-const getInnerTextAsGql = compose(
-  option(null),
-  map(gql),
-  replace(/<!---->/g, ''),
-  prop('innerText')
-);
+import { isGraphQLScript } from './helpers';
 
-const isScriptElement = compose(isSame('SCRIPT'), propOr(null, 'tagName'));
-const isApplicationGraphQL = (el: Element): boolean =>
-  el instanceof Element &&
-  el.getAttribute('type') === 'application/graphql';
+import gql from 'graphql-tag';
 
-/**
- * Throws a TypeError
- */
-function throwTypeError(message: string): never {
-  throw new TypeError(message);
+function getInnerTextAsDocumentNode(x: HTMLElement): DocumentNode | null {
+  const text = x?.innerText?.replace?.(/<!---->/g, '');
+  return text && gql(text);
 }
 
 /**
  * gqlFromInnerText :: (HTMLScriptElement a, DocumentNode b) => a -> b
  * Parses the text content of a graphql script element.
  */
-const gqlFromInnerText: (element: HTMLScriptElement) => DocumentNode | null =
-  ifElse(isNil, constant(null),
-    ifElse(
-      and(isScriptElement, isApplicationGraphQL),
-      getInnerTextAsGql,
-      () => throwTypeError('Script must be of type "application/graphql"')
-    )
-  );
-
-export default gqlFromInnerText;
+export function gqlFromInnerText(element: HTMLScriptElement): DocumentNode | null {
+  if (isGraphQLScript(element))
+    return getInnerTextAsDocumentNode(element);
+  else
+    throw new TypeError('script must be of type "application/graphql"');
+}
