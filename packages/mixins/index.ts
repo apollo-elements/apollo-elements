80365ad295ca9a1c65f713592b09ddecdcc919ab
Benny Powers <web@bennypowers.com>
2020-09-01 19:54:05 +0300

    feat(interfaces): publish interfaces separate pkg
    BREAKING CHANGE: Removes interfaces from mixins package

--- packages/mixins/index.ts
+++ packages/mixins/index.ts
@@ -1,10 +1,10 @@
+import type { ApolloClient, NormalizedCacheObject } from '@apollo/client/core';
+
 export { ApolloElementMixin } from './apollo-element-mixin';
 export { ApolloQueryMixin } from './apollo-query-mixin';
 export { ApolloMutationMixin } from './apollo-mutation-mixin';
 export { ApolloSubscriptionMixin } from './apollo-subscription-mixin';
 
-import type { ApolloClient } from 'apollo-client';
-import { NormalizedCacheObject } from 'apollo-cache-inmemory';
 declare global {
   interface Window {
     __APOLLO_CLIENT__?: ApolloClient<NormalizedCacheObject>;
