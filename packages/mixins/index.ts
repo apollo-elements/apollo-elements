4072b691be948199f72839c61c5edab1a1dae378
Benny Powers <web@bennypowers.com>
2020-09-10 07:52:22 +0300

    feat(mixins): add TypePoliciesMixin
    allows easily lazy-loading type policies.

--- packages/mixins/index.ts
+++ packages/mixins/index.ts
@@ -3,3 +3,4 @@ export { ApolloElementMixin } from './apollo-element-mixin';
 export { ApolloQueryMixin } from './apollo-query-mixin';
 export { ApolloMutationMixin } from './apollo-mutation-mixin';
 export { ApolloSubscriptionMixin } from './apollo-subscription-mixin';
+export { TypePoliciesMixin } from './type-policies-mixin';
---
+++ packages/mixins/type-policies-mixin.ts
@@ -0,0 +1,33 @@
+import type { TypePolicies } from '@apollo/client/core';
+import type { ApolloElementInterface, Constructor } from '@apollo-elements/interfaces';
+import type { Policies } from '@apollo/client/cache/inmemory/policies';
+
+import { dedupeMixin } from '@open-wc/dedupe-mixin';
+
+declare module '@apollo/client/cache' {
+  export abstract class ApolloCache<TSerialized> {
+    policies: Policies;
+  }
+}
+
+function TypePoliciesMixinImpl<B extends Constructor<ApolloElementInterface>>(superclass: B) {
+  return class TypePoliciesElement extends superclass {
+    /**
+     * TypePolicies for the component
+     */
+    declare typePolicies?: TypePolicies;
+
+    constructor(...a: any[]) {
+      super(...a);
+      if (this.typePolicies)
+        this.client.cache.policies.addTypePolicies(this.typePolicies);
+    }
+  };
+}
+
+/**
+ * Lazily adds TypePolicies to the Apollo client when the element connects to the DOM
+ * Provides a `typePolicies` instance property.
+ */
+export const TypePoliciesMixin =
+  dedupeMixin(TypePoliciesMixinImpl);
