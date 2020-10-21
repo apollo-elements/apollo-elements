24aa4009a40d4629e7ee8893591d0add1f591f73
Benny Powers <web@bennypowers.com>
2020-10-21 23:33:08 +0300

    feat(lib): add splitCommasAndTrim string helper
    affects: @apollo-elements/lib

--- packages/lib/helpers.ts
+++ packages/lib/helpers.ts
@@ -26,3 +26,11 @@ const valueIsDefined =
 export function stripUndefinedValues<X>(o: X): X {
   return Object.fromEntries(Object.entries(o).filter(valueIsDefined)) as X;
 }
+
+/** Splits a string by `,` then trims each of the results */
+export function splitCommasAndTrim(value: string): string[] {
+  return value
+    .split(',')
+    .map(x => x.trim())
+    .filter(Boolean);
+}
