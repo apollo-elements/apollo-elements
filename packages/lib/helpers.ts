41c4d4508855568cc9429d925f279be08019d45f
Benny Powers <web@bennypowers.com>
2020-10-24 23:38:56 +0300

    refactor(lib)!: remove DOM helpers
    These will be re-implemented in mixins
    
    affects: @apollo-elements/lib

--- packages/lib/helpers.ts
+++ packages/lib/helpers.ts
@@ -1,25 +1,3 @@
-const scriptSelector = 'script[type="application/graphql"]';
-
-/**
- * Is it a `<script type="application/graphql">`
- */
-export function isGraphQLScript(el: Element): boolean {
-  return (
-    el instanceof HTMLScriptElement &&
-    el.getAttribute('type')?.toLowerCase?.() === 'application/graphql'
-  );
-}
-
-/**
- * Get the child of the element that is a graphql script
- */
-export function getGraphQLScriptChild(el: HTMLElement): HTMLScriptElement {
-  if (!(el instanceof HTMLElement))
-    return null;
-  else
-    return el.querySelector(scriptSelector);
-}
-
 const valueIsDefined =
   ([, v]): boolean => v !== undefined;
 
