07b66f52a4a7b939d4bc6bf966203f43003c3957
Benny Powers <web@bennypowers.com>
2020-11-29 15:32:52 +0200

    chore: update dependencies

--- rollup.config.docs.js
+++ rollup.config.docs.js
@@ -1,13 +1,11 @@
-// import esbuild from 'rollup-plugin-esbuild';
+import esbuild from 'rollup-plugin-esbuild';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
 import litcss from 'rollup-plugin-lit-css';
 import { copy } from '@web/rollup-plugin-copy';
 
 export default {
   input: './packages/docs/components.ts',
-  // external: id => id.includes('packages/docs/'),
   output: {
     format: 'es',
     file: './docs/components.js',
@@ -16,18 +14,17 @@ export default {
     litcss(),
     nodeResolve(),
     commonjs(),
-    typescript({ tsconfig: 'packages/docs/tsconfig.json' }),
     copy({ patterns: '**/*.svg', rootDir: 'packages/docs' }),
-    // esbuild({
-    //   tsconfig: './packages/docs/tsconfig.json',
-    //   include: 'packages/docs/*',
-    //   minify: true,
-    //   sourceMap: true,
-    //   loaders: {
-    //     '.ts': 'ts',
-    //     '.css': 'ts',
-    //     '.graphql': 'ts',
-    //   },
-    // }),
+    esbuild({
+      tsconfig: './packages/docs/tsconfig.json',
+      include: 'packages/docs/*',
+      minify: true,
+      sourceMap: true,
+      loaders: {
+        '.ts': 'ts',
+        '.css': 'ts',
+        '.graphql': 'ts',
+      },
+    }),
   ],
 };
