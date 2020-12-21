5af6576a30570389eeb2a54a3295648053ab9d7c
Benny Powers <web@bennypowers.com>
2020-12-21 09:49:05 +0200

    fix(hybrids): update lib
    affects: @apollo-elements/hybrids

--- packages/hybrids/factories/mutation.ts
+++ packages/hybrids/factories/mutation.ts
@@ -2,7 +2,7 @@ import type { DocumentNode } from '@apollo/client/core';
 import type { Descriptor } from 'hybrids';
 
 import { ApolloMutationElement } from '@apollo-elements/interfaces/apollo-mutation';
-import { apply } from '@apollo-elements/lib/prototypes';
+import { applyPrototype } from '@apollo-elements/lib/prototypes';
 
 import { initDocument } from '../helpers/accessors';
 
@@ -11,7 +11,7 @@ export type { ApolloMutationElement };
 export function mutation<D, V>(document?: DocumentNode): Descriptor<ApolloMutationElement<D, V>> {
   return {
     connect(host, key, invalidate) {
-      apply(host, ApolloMutationElement, 'mutation');
+      applyPrototype<ApolloMutationElement<D, V>>(host, ApolloMutationElement, 'mutation');
       return initDocument<ApolloMutationElement<D, V>>({
         host, document, invalidate, defaults: { called: false },
       });
