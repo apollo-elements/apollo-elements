53f9aa3bb271ac7c62c7041e559999c2f736103e
Benny Powers <web@bennypowers.com>
2020-12-28 19:30:02 +0200

    chore: remove fix-declarations script
    it was out of date and ineffectual anyways

--- packages/lit-apollo/fix-declarations.ts
+++
@@ -1,9 +0,0 @@
-import { readFileSync, writeFileSync } from 'fs';
-
-[
-  './apollo-element.d.ts',
-  './apollo-mutation.d.ts',
-  './apollo-query.d.ts',
-].forEach(path => writeFileSync(path, readFileSync(path, 'utf-8')
-  .replace(/\};\n\} & typeof (.*)Element;/, `} & $1Element;
-} & typeof $1Element; `)));
