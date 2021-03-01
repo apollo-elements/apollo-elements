e6520d72c9634f2f1f9b14cdf198b678927c31e2
Benny Powers <web@bennypowers.com>
2021-03-01 15:51:18 +0200

    style(mixins): export order

--- packages/mixins/index.ts
+++ packages/mixins/index.ts
@@ -1,8 +1,8 @@
-export { ApolloClientMixin } from './apollo-client-mixin';
 export { ApolloElementMixin } from './apollo-element-mixin';
 export { ApolloQueryMixin } from './apollo-query-mixin';
 export { ApolloMutationMixin } from './apollo-mutation-mixin';
 export { ApolloSubscriptionMixin } from './apollo-subscription-mixin';
 
+export { ApolloClientMixin } from './apollo-client-mixin';
 export { TypePoliciesMixin } from './type-policies-mixin';
 export { ValidateVariablesMixin } from './validate-variables-mixin';
