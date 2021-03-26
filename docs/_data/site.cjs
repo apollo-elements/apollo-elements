53d633ae7182f027ef4c55a27dab8cc0820d6973
Benny Powers <web@bennypowers.com>
2021-03-26 16:27:49 +0300

    docs: fix indieauth

--- docs/_data/site.cjs
+++ docs/_data/site.cjs
@@ -1,33 +1,31 @@
-module.exports = async function siteConfig() {
-  return {
-    analytics: 'G-3H2TDNQDBR',
-    dir: 'ltr',
-    lang: 'en',
-    name: 'Apollo Elements',
-    description: 'Build high-performance GraphQL web apps with Apollo Elements',
-    socialLinks: [
+module.exports = {
+   "analytics": "G-3H2TDNQDBR",
+   "dir": "ltr",
+   "lang": "en",
+   "name": "Apollo Elements",
+   "description": "Build high-performance GraphQL web apps with Apollo Elements",
+   "gitSiteUrl": "https://github.com/apollo-elements/apollo-elements",
+   "helpUrl": "https://github.com/apollo-elements/apollo-elements/issues",
+   "logoAlt": "Apollo Elements",
+   "iconColorMaskIcon": "white",
+   "iconColorMsapplicationTileColor": "black",
+   "iconColorThemeColor": "black",
+   "socialLinks": [
       {
-        name: 'GitHub',
-        url: 'https://github.com/apollo-elements/apollo-elements',
+         "name": "GitHub",
+         "url": "https://github.com/apollo-elements/apollo-elements"
       },
       {
-        name: 'Twitter',
-        url: 'https://twitter.com/ApolloElements',
+         "name": "Twitter",
+         "url": "https://twitter.com/ApolloElements"
       },
       {
-        name: 'Discord',
-        url: 'https://discord.gg/2FgMbKf'
+         "name": "Discord",
+         "url": "https://discord.gg/2FgMbKf"
       },
       {
-        name: 'Telegram',
-        url: 'https://t.me/apolloelements'
+         "name": "Telegram",
+         "url": "https://t.me/apolloelements"
       }
-    ],
-    gitSiteUrl: 'https://github.com/apollo-elements/apollo-elements',
-    helpUrl: 'https://github.com/apollo-elements/apollo-elements/issues',
-    logoAlt: '',
-    iconColorMaskIcon: 'white',
-    iconColorMsapplicationTileColor: 'black',
-    iconColorThemeColor: 'black',
-  };
-};
+   ]
+}
