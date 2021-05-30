2cbc2e8f52f881c103d3df7eacc83c7fe16cbf6a
Benny Powers <web@bennypowers.com>
2021-05-30 23:17:31 +0300

    fix(gluon): fix typings

--- packages/gluon/apollo-mutation.test.ts
+++ packages/gluon/apollo-mutation.test.ts
@@ -1,43 +1,29 @@
-import type {
-  ApolloClient,
-  ApolloError,
-  DocumentNode,
-  ErrorPolicy,
-  FetchPolicy,
-  FetchResult,
-  NormalizedCacheObject,
-  TypedDocumentNode,
-} from '@apollo/client/core';
+import type * as C from '@apollo/client/core';
+import type * as I from '@apollo-elements/interfaces';
 
 import type { RefetchQueryDescription } from '@apollo/client/core/watchQueryOptions';
 
-import type { GraphQLError } from '@apollo-elements/interfaces';
-
-import type { MutationElement } from '@apollo-elements/test/mutation.test';
-
 import { ApolloMutation } from './apollo-mutation';
 
-import { assertType, isApolloError } from '@apollo-elements/test';
+import { assertType, isApolloError, stringify, TestableElement } from '@apollo-elements/test';
 import { describeMutation, setupMutationClass } from '@apollo-elements/test/mutation.test';
 import { html } from 'lit-html';
 import { nextFrame } from '@open-wc/testing';
 import { GluonElement } from '@gluon/gluon';
 
-class TestableApolloMutation<D, V> extends ApolloMutation<D, V> implements MutationElement<D, V> {
+class TestableApolloMutation<D extends I.MaybeTDN = I.MaybeTDN, V = I.MaybeVariables<D>>
+  extends ApolloMutation<D, V>
+  implements TestableElement {
   declare shadowRoot: ShadowRoot;
 
   get template() {
     return html`
-      <output id="called">${this.stringify(this.called)}</output>
-      <output id="data">${this.stringify(this.data)}</output>
-      <output id="error">${this.stringify(this.error)}</output>
-      <output id="errors">${this.stringify(this.errors)}</output>
-      <output id="loading">${this.stringify(this.loading)}</output>
+      <output id="called">${stringify(this.called)}</output>
+      <output id="data">${stringify(this.data)}</output>
+      <output id="error">${stringify(this.error)}</output>
+      <output id="errors">${stringify(this.errors)}</output>
+      <output id="loading">${stringify(this.loading)}</output>
     `;
-  }
-
-  stringify(x: unknown) {
-    return JSON.stringify(x, null, 2);
   }
 
   async hasRendered(): Promise<this> {
@@ -46,7 +32,7 @@ class TestableApolloMutation<D, V> extends ApolloMutation<D, V> implements Mutat
     return this;
   }
 
-  $(id: string) { return this.shadowRoot.getElementById(id); }
+  $(id: keyof this) { return this.shadowRoot.getElementById(id as string); }
 }
 
 describe('[gluon] ApolloMutation', function() {
@@ -67,48 +53,48 @@ class TypeCheck extends ApolloMutation<TypeCheckData, TypeCheckVars> {
     assertType<GluonElement>                                  (this);
 
     // ApolloElementInterface
-    assertType<ApolloClient<NormalizedCacheObject>|null>      (this.client);
+    assertType<C.ApolloClient<C.NormalizedCacheObject>|null>  (this.client);
     assertType<Record<string, unknown>>                       (this.context!);
     assertType<boolean>                                       (this.loading);
-    assertType<DocumentNode|null>                             (this.document);
-    assertType<Error|ApolloError|null>                        (this.error);
-    assertType<readonly GraphQLError[]|null>                  (this.errors);
+    assertType<C.DocumentNode|null>                           (this.document);
+    assertType<Error|C.ApolloError|null>                      (this.error);
+    assertType<readonly I.GraphQLError[]|null>                (this.errors);
     assertType<TypeCheckData|null>                            (this.data);
     assertType<string>                                        (this.error!.message);
     assertType<'a'>                                           (this.data!.a);
     // @ts-expect-error: b as number type
     assertType<'a'>                                           (this.data.b);
     if (isApolloError(this.error!))
-      assertType<readonly GraphQLError[]>                     (this.error.graphQLErrors);
+      assertType<readonly I.GraphQLError[]>                   (this.error.graphQLErrors);
 
     // ApolloMutationInterface
-    assertType<DocumentNode|null>                             (this.mutation);
+    assertType<C.DocumentNode|null>                           (this.mutation);
     assertType<TypeCheckVars|null>                            (this.variables);
     assertType<boolean>                                       (this.called);
     assertType<boolean>                                       (this.ignoreResults!);
     assertType<boolean>                                       (this.awaitRefetchQueries!);
-    assertType<ErrorPolicy>                                   (this.errorPolicy!);
+    assertType<C.ErrorPolicy>                                 (this.errorPolicy!);
     assertType<string>                                        (this.errorPolicy!);
     // @ts-expect-error: ErrorPolicy is not a number
     assertType<number>                                        (this.errorPolicy);
     assertType<string>                                        (this.fetchPolicy!);
-    assertType<Extract<FetchPolicy, 'no-cache'>>              (this.fetchPolicy);
+    assertType<Extract<C.FetchPolicy, 'no-cache'>>            (this.fetchPolicy);
 
     if (typeof this.refetchQueries === 'function')
-      assertType<(result: FetchResult<TypeCheckData>) => RefetchQueryDescription>(this.refetchQueries);
+      assertType<(result: C.FetchResult<TypeCheckData>) => RefetchQueryDescription>(this.refetchQueries);
     else
       assertType<RefetchQueryDescription|null>                (this.refetchQueries);
 
     if (typeof this.optimisticResponse !== 'function')
       assertType<TypeCheckData>                               (this.optimisticResponse!);
     else
-      assertType<(vars: TypeCheckVars) => TypeCheckData>(this.optimisticResponse);
+      assertType<(vars: TypeCheckVars) => TypeCheckData>      (this.optimisticResponse);
 
     /* eslint-enable max-len, func-call-spacing, no-multi-spaces */
   }
 }
 
-type TDN = TypedDocumentNode<TypeCheckData, TypeCheckVars>;
+type TDN = C.TypedDocumentNode<TypeCheckData, TypeCheckVars>;
 class TypeCheckTDN extends ApolloMutation<TDN> {
   typeCheck() {
     assertType<TypeCheckData>(this.data!);
--- packages/gluon/apollo-subscription.test.ts
+++ packages/gluon/apollo-subscription.test.ts
@@ -7,28 +7,27 @@ import { html } from 'lit-html';
 import { nextFrame } from '@open-wc/testing';
 
 import { ApolloSubscription } from './apollo-subscription';
-import { assertType, isApolloError } from '@apollo-elements/test';
+import { assertType, isApolloError, stringify, TestableElement } from '@apollo-elements/test';
 
 import {
   describeSubscription,
   setupSubscriptionClass,
-  SubscriptionElement,
 } from '@apollo-elements/test/subscription.test';
 
 import { GluonElement } from '@gluon/gluon';
 
 class TestableApolloSubscription<D extends I.MaybeTDN = I.MaybeTDN, V = I.MaybeVariables<D>>
   extends ApolloSubscription<D, V>
-  implements SubscriptionElement<D, V> {
+  implements TestableElement {
   declare shadowRoot: ShadowRoot;
 
   static get is() { return 'gluon-test-subscription-element'; }
 
   get template() {
     return html`
-      <output id="data">${this.stringify(this.data)}</output>
-      <output id="error">${this.stringify(this.error)}</output>
-      <output id="loading">${this.stringify(this.loading)}</output>
+      <output id="data">${stringify(this.data)}</output>
+      <output id="error">${stringify(this.error)}</output>
+      <output id="loading">${stringify(this.loading)}</output>
     `;
   }
 
@@ -36,9 +35,7 @@ class TestableApolloSubscription<D extends I.MaybeTDN = I.MaybeTDN, V = I.MaybeV
     return this.shadowRoot.getElementById(id as string);
   }
 
-  stringify(x: unknown) { return JSON.stringify(x, null, 2); }
-
-  async hasRendered(): Promise<TestableApolloSubscription<D, V>> {
+  async hasRendered(): Promise<this> {
     await this.updateComplete;
     await nextFrame();
     await this.render();
@@ -64,29 +61,29 @@ class TypeCheck extends ApolloSubscription<TypeCheckData, TypeCheckVars> {
 
     // ApolloElementInterface
     assertType<C.ApolloClient<C.NormalizedCacheObject>> (this.client!);
-    assertType<Record<string, unknown>>             (this.context!);
-    assertType<boolean>                             (this.loading);
-    assertType<C.DocumentNode>                        (this.document!);
-    assertType<Error>                               (this.error!);
-    assertType<readonly I.GraphQLError[]>             (this.errors!);
-    assertType<TypeCheckData>                       (this.data!);
-    assertType<string>                              (this.error.message);
-    assertType<'a'>                                 (this.data.a);
+    assertType<Record<string, unknown>>                 (this.context!);
+    assertType<boolean>                                 (this.loading);
+    assertType<C.DocumentNode>                          (this.document!);
+    assertType<Error>                                   (this.error!);
+    assertType<readonly I.GraphQLError[]>               (this.errors!);
+    assertType<TypeCheckData>                           (this.data!);
+    assertType<string>                                  (this.error.message);
+    assertType<'a'>                                     (this.data.a);
     // @ts-expect-error: b as number type
-    assertType<'a'>                                 (this.data.b);
+    assertType<'a'>                                     (this.data.b);
     if (isApolloError(this.error))
-      assertType<readonly I.GraphQLError[]>           (this.error.graphQLErrors);
+      assertType<readonly I.GraphQLError[]>             (this.error.graphQLErrors);
 
     // ApolloSubscriptionInterface
     assertType<C.DocumentNode>                          (this.subscription!);
-    assertType<TypeCheckVars>                         (this.variables!);
+    assertType<TypeCheckVars>                           (this.variables!);
     assertType<C.FetchPolicy>                           (this.fetchPolicy!);
     assertType<C.ErrorPolicy>                           (this.errorPolicy!);
-    assertType<string>                                (this.fetchPolicy);
-    assertType<boolean>                               (this.notifyOnNetworkStatusChange!);
-    assertType<number>                                (this.pollInterval!);
-    assertType<boolean>                               (this.skip);
-    assertType<boolean>                               (this.noAutoSubscribe);
+    assertType<string>                                  (this.fetchPolicy);
+    assertType<boolean>                                 (this.notifyOnNetworkStatusChange!);
+    assertType<number>                                  (this.pollInterval!);
+    assertType<boolean>                                 (this.skip);
+    assertType<boolean>                                 (this.noAutoSubscribe);
 
     /* eslint-enable max-len, func-call-spacing, no-multi-spaces */
   }
