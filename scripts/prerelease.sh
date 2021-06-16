8887bade907dd395256956bb365e9719eff01c5a
Benny Powers <web@bennypowers.com>
2021-06-16 21:25:24 +0300

    chore: prerelease changesets since next

--- scripts/prerelease.sh
+++ scripts/prerelease.sh
@@ -5,6 +5,6 @@
   && yarn changeset pre enter next \
   || echo 'Already in pre mode'
 
-yarn changeset version
+yarn changeset version --since next
 git add .
 git commit -m "chore: version prerelease packages"
