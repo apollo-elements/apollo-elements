bf812f894dd6c7e848f4c6288a0784c16445e5fc
Benny Powers <web@bennypowers.com>
2021-06-17 20:29:25 +0300

    fix: ignore .d.ts in generated custom-elements-manifest

---
+++ .changeset/healthy-drinks-confess.md
@@ -0,0 +1,7 @@
+---
+"@apollo-elements/components": patch
+"@apollo-elements/core": patch
+"@apollo-elements/lit-apollo": patch
+---
+
+Fix generated custom elements manifest
--- packages/mixins/custom-elements-manifest.config.js
+++ packages/mixins/custom-elements-manifest.config.js
@@ -1,8 +1,10 @@
 import { plugins, copyPlugin } from '../docs/custom-elements-manifests-plugins/index.js';
 
-export default {
+/** @type {import('@open-wc/custom-elements-manifest').Plugin} */
+export default ({
+  exclude: ['*.d.ts'],
   plugins: [
     ...plugins,
     copyPlugin(import.meta.url),
   ],
-};
+});
