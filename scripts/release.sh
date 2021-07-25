0db0ce2e7e1c104311074eed91f24445dce52f73
Benny Powers <web@bennypowers.com>
2021-07-25 22:08:20 +0300

    chore: use problem matchers instead of bespoke actions
    Thanks, @abdonrd

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-# not quite ready for that
-# npx changeset pre exit
-
+# Thanks, Westbrook
+[ ! -f .changeset/pre.json ] && npx changeset pre exit 
 npm run clean
-# npm run --workspaces --if-present prepublishOnly
-
 npx changeset publish
