fab48d27b8c30fc25f72a424620ece352b951e79
Benny Powers <web@bennypowers.com>
2021-07-26 22:44:10 +0300

    chore: try to fix ci

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-[ -z ${NPM_TOKEN+x} ] && cat << EOF > ".npmrc"
-  @apollo-elements:registry=//registry.npmjs.org/:_authToken=$NPM_TOKEN
-  //registry.npmjs.org/:_authToken=$NPM_TOKEN
-EOF;
-
 # Thanks, Westbrook
 [ ! -f .changeset/pre.json ] && npx changeset pre exit
 npm run clean
