05daa4248efb6afb8ebe56d9d48c813d583b28b8
Benny Powers <web@bennypowers.com>
2021-07-30 00:12:10 +0300

    docs: update apollo client

---
+++ docs/blog/next-evolution/hybrids-healthy-snack.js
@@ -0,0 +1,16 @@
+import { query, html, define } from '@apollo-elements/hybrids';
+import { client } from './client.js';
+import { FruitsQuery } from './Fruits.query.graphql.js';
+import { VeggiesQuery } from './Veggies.query.graphql.js';
+
+define('healthy-snack', {
+  fruits: query(FruitsQuery, { client }),
+  veggies: query(VeggiesQuery, { client }),
+  render(host) {
+    const snack = [ ...host.fruits.data?.fruits ?? [], ...host.veggies.data?.veggies ?? [] ];
+    return html`
+      <link rel="stylesheet" href="healthy-snack.css"/>
+      <ul>${snack.map(x => html`<li>${x}</li>`)}</ul>
+    `;
+  }
+});
