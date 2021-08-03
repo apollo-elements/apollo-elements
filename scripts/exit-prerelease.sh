dc7c04c95e43e36a4b2fb8378da025455723832d
Benny Powers <web@bennypowers.com>
2021-08-03 22:01:06 +0300

    chore: boolean error

--- scripts/exit-prerelease.sh
+++ scripts/exit-prerelease.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Thanks, Westbrook
-if [ ! -f .changeset/pre.json ]; then
+if [ -f .changeset/pre.json ]; then
   npx changeset pre exit
   git add .changeset/pre.json
   git commit --message "chore: exit prerelease"
