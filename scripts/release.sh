ee61059ef1b495a48bf9f60d99c575c5fa0a3c99
Benny Powers <web@bennypowers.com>
2021-08-03 22:57:57 +0300

    chore: manually exit pre mode 🤷‍♂️

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
+# Thanks, Westbrook
+if [ -f .changeset/pre.json ]; then
+  npx changeset pre exit
+else
+  echo "Not in prerelease mode"
+fi
+
 npm run clean
 npx changeset publish
