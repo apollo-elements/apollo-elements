694e2f1ffeccde63487c186174f2a33b8f8ba937
Benny Powers <web@bennypowers.com>
2021-08-03 22:46:23 +0300

    chore: commit pre exit

--- scripts/exit-prerelease.sh
+++ scripts/exit-prerelease.sh
@@ -3,7 +3,10 @@
 # Thanks, Westbrook
 if [ -f .changeset/pre.json ]; then
   npx changeset pre exit
+  git config user.name "github-actions[bot]"
+  git config user.email "github-actions[bot]@users.noreply.github.com"
   git add .changeset/pre.json
+  git commit --message "chore: exit prerelease"
 else
   echo "Not in prerelease mode"
 fi
